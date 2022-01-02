831f6cf913d64179e29faede3216dbf88826a5bb
minjun-P <jun7332568@gmail.com>
2022-01-02 22:19:47 +0900

    1.2 22:29, 집 돌아와서 추가 작업

--- lib/profile.dart
+++ lib/profile.dart
@@ -21,8 +21,11 @@ class _ProfilePageState extends State<ProfilePage> with SingleTickerProviderStat
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('짱구의 프로필',
+        title: Text(
+          '짱구의 프로필',
+          style: TextStyle(color: Colors.black),
         ),
+        centerTitle: true,
 
       ),
       body: Column(
@@ -30,15 +33,25 @@ class _ProfilePageState extends State<ProfilePage> with SingleTickerProviderStat
         children: [
           SizedBox(height: 14,),
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              SizedBox(
-                width: 80,
-                height: 80,
-                child: CircleAvatar(
-                  backgroundColor: Colors.grey[300],
-                  backgroundImage: NetworkImage('https://upload.wikimedia.org/wikipedia/ko/4/4a/%EC%8B%A0%EC%A7%B1%EA%B5%AC.png'),
+              Column(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey[300],
+                      backgroundImage: NetworkImage('https://upload.wikimedia.org/wikipedia/ko/4/4a/%EC%8B%A0%EC%A7%B1%EA%B5%AC.png'),
+                  ),
                 ),
+                  SizedBox(height: 5,),
+                  Text(
+                    '신짱구',
+                    style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),
+                  )
+                ]
               ),
               _buildInfo('팔로잉','10'),
               _buildInfo('팔로우','12'),
@@ -58,8 +71,30 @@ class _ProfilePageState extends State<ProfilePage> with SingleTickerProviderStat
             child: TabBarView(
               controller: _tabController,
               children: [
-                Container(color: Colors.red,),
-                Container(color: Colors.green,),
+                ListView(
+                  children: List.generate(10,(index) {
+                    return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                        child: Container(
+                          height: 100,
+                          color: Colors.amberAccent,
+                          child: Text('내가 쓴 일기'),
+                        )
+                    );
+                  })
+                ),
+                ListView(
+                    children: List.generate(10,(index) {
+                      return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                          child: Container(
+                            height: 100,
+                            color: Colors.greenAccent,
+                            child: Text('내가 좋아요 한 일기'),
+                          )
+                      );
+                    })
+                ),
               ],
             ),
           )
@@ -74,6 +109,7 @@ class _ProfilePageState extends State<ProfilePage> with SingleTickerProviderStat
   _buildInfo(String title, String count) {
     return Column(
       children: [
+        SizedBox(height: 8,),
         Text(
           title,
           style: TextStyle(fontSize: 20),
